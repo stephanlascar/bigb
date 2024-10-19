6fca05c06e0bc572b42cf291c24fbd8d1ee6c8f7
Stéphan Lascar <stephan@lascar.im>
2024-10-19 17:42:45 +0200

    Convert date to local user datetime

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import datetime
 import os
 
 import psycopg
+import pytz
 from flask import Flask, request
 from flask_cors import CORS
 from flask_pydantic import validate
@@ -27,11 +29,17 @@ def add_log_event(body: AddLogEventModel):
     if api_key != API_KEY:
         return "", 401
 
+    parsed_date = datetime.datetime.fromisoformat(body.date[:-1])
+    utc_timezone = pytz.timezone('UTC')
+    aware_date = parsed_date.replace(tzinfo=utc_timezone)
+
+    local_datetime = aware_date.astimezone(pytz.timezone(body.timezone)).replace(tzinfo=None)
+
     with psycopg.connect(DATABASE_URI) as conn:
         with conn.cursor() as cursor:
             cursor.execute(
                 "INSERT INTO event_log (device_id, local_datetime, title, url) VALUES (%s, %s, %s, %s)",
-                (body.id, body.date, body.title, body.url))
+                (body.id, local_datetime, body.title, body.url))
 
             conn.commit()
 
