a6112bdde0ae85c7c2eb9205423792338bd8b087
Stéphan Lascar <stephan@lascar.im>
2024-10-19 17:29:26 +0200

    Add flask cors

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 import os
 
 import psycopg
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
 from flask_pydantic import validate
 
 from models import AddLogEventModel
 
 DATABASE_URI = os.environ.get("DATABASE_URL")
+API_KEY = os.environ.get("API_KEY", "You will never guess!")
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/')
@@ -19,6 +22,11 @@ def hello_world():
 @app.route('/api/events/logs/add', methods=['POST'])
 @validate()
 def add_log_event(body: AddLogEventModel):
+    headers = request.headers
+    api_key = headers.get("X-Api-Key")
+    if api_key != API_KEY:
+        return "", 401
+
     with psycopg.connect(DATABASE_URI) as conn:
         with conn.cursor() as cursor:
             cursor.execute(
